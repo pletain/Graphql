0c3e903c1c531bd12de7c404f384633083c78341
김태형 <thk12378905@gmail.com>
2021-06-19 10:39:59 +0000

    modify

--- graphql/index.js
+++ graphql/index.js
@@ -25,6 +25,7 @@ const schema = buildSchema(`
 
 	type Mutation {
 				addProduct( input : ProductInput ) : Product
+				updateProduct( id : ID! , input : ProductInput! ) : Product
 	}
 
 `);
@@ -42,15 +43,27 @@ const products = [{
 	description : 'apple'
 }]
 
+
 const root = { 
 	getProduct : ({ id }) => products.find( product => product.id == parseInt(id) ),
-	
+	//input에서 넘어온 데이터를 데이터베이스에 추가함
+	//id는 직접 추가해줄 줌
 	addProduct : ({ input }) => {
 			input.id = parseInt(products.length + 1);
 			products.push(input);
 			return root.getProduct({ id : input.id })
+	},
+	updateProduct : ({ id , input }) => { //여기의 id값이
+			const index = products.findIndex(product => product.id === parseInt(id) ) //여기로 갔을 때, 일치하는 값을 조회
+			products[index]	= { //덮어 씌우기한다.
+					id : parseInt(id),
+					...input //안에서 입력됐던 값들이 펼쳐짐
+			}
+			return products[index];
 	}
 };
+
+
 
 const app = express();
 app.use( '/graphql', graphqlHTTP({
